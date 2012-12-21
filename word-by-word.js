3f3c66a6ec6d96967dd3102359ac97263e95af19
Jordan Scales <scalesjordan@gmail.com>
2012-12-21 15:19:51 -0500

    undo action, error checking

--- word-by-word.js
+++ word-by-word.js
@@ -3,18 +3,26 @@ Players = new Meteor.Collection("players");
 
 if (Meteor.isClient) {
   Template.arena.words = function() {
-    return Words.find({});
+    return Words.find({}, {sort: {timestamp: 1}});
   };
 
   Template.arena.events({
     'keydown input.text': function(event) {
       if (event.which == 13) {
-        Words.insert({
-                      content: event.target.value, 
-                      timestamp: (new Date()).getTime()
-                    });
-        event.target.value = '';
+        if (event.target.value.match(/\s+/)) {
+          $('#error').text('One word only! (no whitespace)').show().fadeOut(2000);
+        } else {
+          Words.insert({
+                        content: event.target.value, 
+                        timestamp: (new Date()).getTime()
+                      });
+          event.target.value = '';
+        }
       }
+    },
+
+    'click input.undo': function(event) {
+      Words.remove(Words.findOne({}, {sort: {timestamp: -1}}));
     }
   });
 }
